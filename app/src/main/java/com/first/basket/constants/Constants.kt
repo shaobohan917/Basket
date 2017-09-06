1ee9c9e28065c9c8f22aec099502dce7f0cd662e
hanshaobo <hanshaobo@021.com>
2017-09-06 09:34:35 +0800

    add git

--- app/src/main/java/com/first/basket/constants/Constants.kt
+++ app/src/main/java/com/first/basket/constants/Constants.kt
@@ -4,7 +4,9 @@ package com.first.basket.constants
  * Created by hanshaobo on 02/09/2017.
  */
 class Constants {
+    var git = "git push https://github.com/LukaHan/Basket.git"
+
     companion object {
-        public val PIC_URL: String = "http://07.imgmini.eastday.com/mobile/20170831/20170831164106_cd214bb067a8c130816b5827b4231ac0_1.jpeg"
+        val PIC_URL: String = "http://07.imgmini.eastday.com/mobile/20170831/20170831164106_cd214bb067a8c130816b5827b4231ac0_1.jpeg"
     }
 }
